d4ba14864ebadb4ad2399c285019c3abf71ddf72
TeknicalMage <arcazjordan@gmail.com>
2021-11-08 01:45:00 -0500

    IT WORKS ON OTHER CRYPTOSS AHHHHHHHHHHHH
    So you can change the coin and it will still get the value (IF THE COIN HAS A FLUCATING VALUE) (IT WILL ONLY FUNCTION FROM AN OVERVIEW PAGE).

--- watchprice.py
+++ watchprice.py
@@ -14,80 +14,97 @@ from datetime import datetime
  
 
 import string
-
-
-
 import pickle
-
-
-#Clicking Google Search button 
-#driver.find_element_by_xpath("//input[@value='Google Search']").click()
-
-#Keyword searched in the chrometest
-
-x = 1
 
 
 
 def chrometest(): 
     compval = "base"
     macrovalue = ""
+    macrostr = ""
+    x = 0
     chrome_options = Options()
 
     #Keyword searched in the chrometest
 
+    #You need a chrome profile logged into coinbase. Any basic account will do. The goal is just to utilize the stats
+    #displayed and updated by coinbase without refreshing the page or being flagged as a bot
+    #Having a logged in account minimizes site interaction, which lowers the chance of detection
     chrome_options.add_argument('--user-data-dir=C:/Users/arcaz/Documents/Programming Projects/ShibTrackr/User Data2')
-
     chrome_options.add_argument('--profile-directory=Default')
 
+
+    #Gets rid of errors \ selenium hook log bs
     chrome_options.add_argument('--log-level=3')
+    #Gets rid of errors \ selenium hook log bs
+    
     chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    chrome_options.add_argument("--disable-extensions")
+    #chrome_options.add_argument("--disable-gpu")
+    #chrome_options.add_argument("--headless")
 
 
     driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.coinbase.com/price/shiba-inu")
-    
-    #driver.find_element_by_xpath("//*[@name='q']").send_keys(searchtext)  
-    #driver.find_element_by_xpath("//*[@name='q']").send_keys(Keys.ENTER)  
+
+    #Default selection
+    driver.get("https://www.coinbase.com/price/bitcoin")
+    #Change this to whatever you want, or just change the site manually from the opened chrome window
+      
     print('test')
     time.sleep(5)
     
     
     while True:
-        now = datetime.now()
-        val1 = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[0].get_attribute('innerText')
-        val2 = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[1].get_attribute('innerText')
-        val3 = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[2].get_attribute('innerText')
-        val4 = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[3].get_attribute('innerText')
-        val5 = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[4].get_attribute('innerText')
-        val6 = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[5].get_attribute('innerText')
-        val7 = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[6].get_attribute('innerText')
-        val8 = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[7].get_attribute('innerText')
-        val9 = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[8].get_attribute('innerText')
-        val10 = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[9].get_attribute('innerText')
-        time.sleep(.2)
-        bignumber = (val1+val2+val3+val4+val5+val6+val7+val8+val9+val10)
+        try:
+            now = datetime.now()
 
-        
 
-        macrovalue = str(bignumber)
-        
+            #How many times to iterate over the actual price
+            cntvalue = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__Container-sc-1q68ya3-0 dgxbSY"]')
+            maco = (len(cntvalue))
+            #How many times to iterate over the actual price
 
-        if compval == macrovalue:
+
+            #In case pages are swapped, current coin value you are getting
+            listr = str(driver.current_url)
+            listrnd = (listr.replace("https://www.coinbase.com/price/", ""))
+            
+
+                        
+            for x in range(maco):
+                
+                val = driver.find_elements_by_xpath('//*[@class="Flex-l69ttv-0 PriceTickerChar__ValueContainer-sc-1q68ya3-2 WBuZA"]')[x].get_attribute('innerText')
+                #Adding everything to a larger string
+                macrostr+=val
+
+            
+            #functionally useless, but it merges into the og setup
+            macrovalue = str(macrostr)
+            macrostr = ""
+            
+            #Look at the value, if it isn't new, don't do anything
+            if compval == macrovalue:
+                pass
+            else:
+                print('---------')
+                #Makes sure it won't repeat in the next iteration
+                compval = macrovalue    
+
+                #Functional output of the program
+                print(listrnd)
+                print('$' + macrovalue, end="")
+                print (" | " + now.strftime("%Y-%m-%d %H:%M:%S"))\
+                
+        except:
             pass
-        else:
-            print('---------')
-            compval = macrovalue
-            print('$' + macrovalue, end="")
-            print (" | " + now.strftime("%Y-%m-%d %H:%M:%S"))
 
     time.sleep(10000)
 
 
-        #print("---")
-        #print(compval)  
-        #print(macrovalue)   
-        #print("---")
+            #print("---")
+            #print(compval)  
+            #print(macrovalue)   
+            #print("---")
 
 chrometest()
